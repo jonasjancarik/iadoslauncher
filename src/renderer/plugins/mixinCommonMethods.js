25f4d664a37d73f9e153491b94c921946e37080f
Jonáš Jančařík <jonas.jancarik@gmail.com>
2021-11-18 15:05:36 +0100

    improved visual, functionality, new mixins
    add methods

--- src/renderer/plugins/mixinCommonMethods.js
+++ src/renderer/plugins/mixinCommonMethods.js
@@ -1,10 +1,20 @@
 import { exec } from 'child_process'
 import Vue from 'vue'
+// import { remote } from 'electron'
+import { shell } from 'electron'
+
 const fs = require('fs')
+const platform = require('os').platform()
 
 Vue.mixin({
   methods: {
-    checkDosBoxInstallation () {
+    openURL (url) {
+      shell.openExternal(url)
+    },
+    getDosboxInstallPath () {
+      // this checks if the user has a dosbox installed at the path defined in settings
+      // returns the path if found, otherwise returns null
+      console.log('getDosboxInstallPath called')
       const platform = require('os').platform()
       if (platform === 'win32') {
         // look for DOSBox installation in the default location
@@ -22,6 +32,8 @@ Vue.mixin({
           } else {
             return null
           }
+        } else {
+          return null
         }
       } else {
         exec(`which ${this.settings.dosBoxExePath.value}`, (error, stdout, stderr) => {
@@ -38,6 +50,45 @@ Vue.mixin({
           }
         })
       }
+    },
+    checkPackageManagerAvailability () {
+      return new Promise((resolve, reject) => {
+        console.log('Checking package manager availability')
+        if (platform === 'win32') {
+          exec('winget', (error, stdout, stderr) => {
+            if (error || stderr) {
+              console.log(`error: ${error.message}`)
+              reject(new Error('Package manager not found'))
+            } else if (stdout) {
+              console.log('winget package manager available')
+              resolve(true)
+            }
+          })
+        } else {
+          return true // todo: check for mac and linux
+        }
+      })
+    },
+    installDosbox () {
+      return new Promise((resolve, reject) => {
+        if (platform === 'win32') {
+          exec('winget install -e --id DOSBox.DOSBox', (error, stdout, stderr) => {
+            if (error) {
+              console.log(`error: ${error.message}`)
+              reject(error.message)
+            } else if (stderr) {
+              console.log(`stderr: ${stderr}`)
+              reject(stderr)
+            } else {
+              console.log(`stdout: ${stdout}`)
+              resolve(stdout)
+            }
+          })
+        } else {
+          console.log('installDosbox: not implemented')
+          return null
+        }
+      })
     }
   }
 })
